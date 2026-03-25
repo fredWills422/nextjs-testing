1ab828c896e5d1c991ade4d13a8be1ef871d3551
Fred Wills <willsf422@gmail.com>
2026-03-25 15:53:08 -0400

    heading + image test

---
+++ base-concert-venue/__tests__/ui/home.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Home from '@/pages/index';
+
+test('page has correct heading and image', () => {
+    
+    render(<Home />);
+    const heading = screen.getByRole(
+        'heading', { name: /welcome to popular concert venue/i }
+    );
+    expect(heading).toBeInTheDocument();
+    const image = screen.getByRole(
+        'img', { name: /concert goer with hands in the shape of a heart/i }      
+    );
+    expect(image).toBeInTheDocument();
+
+});
